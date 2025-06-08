4e04e1c73b76125953f0ee267d96450481ba8332
fatimashbib <64587505+fatimashbib@users.noreply.github.com>
2025-06-08 09:24:49 +0300

    env changes

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    port: 5173,
+    strictPort: true,
+    allowedHosts: [".csb.app"], // 👈 allow all subdomains of CodeSandbox
+  },
 });
